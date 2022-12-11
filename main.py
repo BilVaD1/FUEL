7c6bd29f545e208f6aed6df03c7c48f3d0924bc4
Vadym <bilvad1@gmail.com>
2022-12-11 17:49:06 +0200

    Added calculation by temperature

--- main.py
+++ main.py
@@ -44,6 +44,14 @@ def return_sounding():
     content = Sounding_entry.get()
     return content
 
+def return_density():
+    content = Density_entry.get()
+    return content
+
+def return_temperature():
+    content = Temperature_entry.get()
+    return content
+
 
 root = Tk()
 root.title('Tanks')
@@ -83,15 +91,28 @@ answer_m3 = Label(root, text='Results: ')
 answer_m3.grid(row=2, column=2, sticky=W)
 
 answer_m3 = Label(root, padx=5, text='There will be the answer in m3')
-answer_m3.grid(row=2, column=3, sticky=NE, pady=15)
-answer_t = Label(root, padx=5, text='There will be the answer in t')
-answer_t.grid(row=2, column=3, sticky=SE, pady=35)
+answer_m3.grid(row=2, column=3, sticky=E, pady=15)
 
+Density_label = Label(root, text='Please write the density of fuel at 15 celsius:')
+Density_label.grid(row=3, column=0, padx=10, pady=10, sticky=W)
 
+Density_entry = Entry(root)
+Density_entry.grid(row=3, column=2, columnspan=2, padx=10, sticky=W + E)
+
+Temperature_label = Label(root, text='Please write the current temperature of fuel:')
+Temperature_label.grid(row=4, column=0, padx=10, pady=10, sticky=W)
+
+Temperature_entry = Entry(root)
+Temperature_entry.grid(row=4, column=2, columnspan=2, padx=10, sticky=W + E)
+
+answer_mass = Label(root, padx=5, text='There will be the mass of oil in "t"')
+answer_mass.grid(row=5, column=2, sticky=W + E, pady=35)
+
+tanks_for_colors = [] # This is variable for realize the changing of color on each curve of the graphic, it keeps only unique values
 '''Function for calling class Helpers()'''
 def task2():
     tasks = Tasks()
-    return tasks.task1(return_sounding(), return_tank_name(), answer_t, answer_m3)
+    return tasks.task1(return_sounding(), return_tank_name(), return_density(), return_temperature(), answer_mass, answer_m3, tanks_for_colors)
 
 
 Start = Button(root, command=task2, text='Start')
--- tasks.py
+++ tasks.py
@@ -1,4 +1,5 @@
 import re
+import random
 from openpyxl import load_workbook
 import matplotlib.pyplot as plt  # Библиотека для работы с графиками
 import numpy as np
@@ -10,35 +11,68 @@ class Tasks:
         # Load in the workbook
         self.wb = load_workbook('./data/Book1.xlsx')
 
-    def createGraph(self, x, y, z, z2):
-        plt.plot(x, y, color="red", marker="o", ms=5, label="label")  # Создаем график с переменными в качестве x, y
-        plt.plot(z, z2, color="blue", marker="o", ms=5, label="measurements")  # Создаем точку вычислений на графике
+    def createGraph(self, x, y, z, z2, tankName, sounding, tanks_for_colors):
+        colors = ["Blue", "Green", "Cyan", "Magenta", "Yellow", "Black", "Pink", "Purple", "Orange", "Gray", "Brown", "Beige"]
+        if len(tanks_for_colors) == 1:
+            color="red"
+        elif len(tanks_for_colors) > 1:
+            color = random.choice(colors)
+        plt.plot(x, y, color=color, marker="o", ms=5, label=f'Tank: {tankName}')  # Создаем график с переменными в качестве x, y
+        plt.plot(z, z2, color="blue", marker="o", ms=5, label=f"Volume on {sounding}m")  # Создаем точку вычислений на графике
         plt.legend()
         plt.xlabel("$deep(m)$", fontsize=16)
         plt.ylabel("$Vm^3$", fontsize=16)
         plt.show()
 
-    def writeValuesToExcel(self, sheet, z, z2, t):
+    def writeValuesToExcel(self, sheet, z, z2):
         sheet.cell(row=3, column=7, value=z)
         sheet.cell(row=4, column=7, value=z2)
-        sheet.cell(row=5, column=7, value=t)
         self.wb.save(filename='./data/Book1.xlsx')
 
-    def task1(self, sounding, tank, answer_t, answer_m3):
+    def writeMassToExcel(self, sheet, mass):
+        sheet.cell(row=5, column=7, value=mass)
+        self.wb.save(filename='./data/Book1.xlsx')
 
+    def temperatureCorrectedDensity(self, density, currentTemperature):
+        correctedDensity = density * (1 - (0.00064*(currentTemperature - 15)))
+        return correctedDensity
+    
+    def mass(self, density, currentTemperature, volume):
+        mass = self.temperatureCorrectedDensity(density, currentTemperature) * volume
+        return mass
+
+    def displayMass(self, density, currentTemperature, z, answer_mass, sheet):
+        currentMass = self.mass(density, currentTemperature, z)
+        answer_mass['text'] = f'You have {"{0:.3f}".format(currentMass)} t' # {"{0:.3f}".format(z)} use only 2 numbers after the dot in the t
+        self.writeMassToExcel(sheet, currentMass)
+        return answer_mass
+
+
+
+    def task1(self, sounding, tank, density, currentTemperature, answer_mass, answer_V, tanks_for_colors):
+        '''These are the statements for fields validations'''
         if sounding == '' and tank == '':
-            answer_t['text'] = ''
-            answer_m3['text'] = 'Please enter values into the fields'
-            return answer_t, answer_m3
+            answer_mass['text'] = ''
+            answer_V['text'] = 'Please enter values into the fields'
+            return answer_mass, answer_V
         elif tank == '':
-            answer_m3['text'] = 'Please enter the valid value into the Tank name field'
-            return answer_m3
+            answer_V['text'] = 'Please enter the valid value into the Tank name field'
+            return answer_V
         elif sounding == '':
-            answer_m3['text'] = 'Please enter the valid value into the Sounding field'
-            return answer_m3
+            answer_V['text'] = 'Please enter the valid value into the Sounding field'
+            return answer_V
         elif bool(re.search(r'[^\W\d]', sounding)) == True:   # Check the string on the letters
-            answer_m3['text'] = 'Please enter the valid value into the Sounding field'
-            return answer_m3
+            answer_V['text'] = 'Please enter the valid value into the Sounding field'
+            return answer_V
+        elif density == '' and currentTemperature == '':
+            calcMass = False
+        elif density != '' and currentTemperature != '':
+            calcMass = True
+            density = float(density)
+            currentTemperature = float(currentTemperature)
+        elif density != '' or currentTemperature != '':
+            calcMass = False
+            answer_mass['text'] = 'Specify density and temperature'
 
         sounding_number = float(sounding)
         tank_name = str(tank)
@@ -54,8 +88,8 @@ class Tasks:
             a = re.search(tank_name, l1, re.IGNORECASE).group()  # Передаем в переменную искаемое слово с помощью group()
             sheet = self.wb[a]  # Get a sheet by name
         else:
-            answer_m3['text'] = 'Uncorrected name of tank!'
-            return answer_m3
+            answer_V['text'] = 'Uncorrected name of tank!'
+            return answer_V
 
         # Создаем списки чтобы в них поместить координаты
         x = []
@@ -74,7 +108,7 @@ class Tasks:
                 y.append(y1)
             else:
                 continue
-        k = sheet.cell(row=2, column=7).value  # Получаем значение плотности из таблицы
+        k = sheet.cell(row=2, column=7).value  # Получаем значение корректирующего фактора из таблицы
 
         z_max = x[-1]  # Находим последнее значение списка глубины, чтобы оптимизировать работу на всех страницах Екселя
 
@@ -82,13 +116,20 @@ class Tasks:
 
         if 0.00 < sounding_number < z_max:
             z = float(np.interp(sounding_number, x, y))  # Считаем с помощью интерпритаций значение точки на графике, в нашем случае z2
-            answer_m3['text'] = f'You have {"{0:.3f}".format(z)} m3'  # {"{0:.3f}".format(z)} use only 2 numbers after the dot in the z2
-            t = k * z  # Вычисленние тонн
-            answer_t['text'] = f'You have {"{0:.3f}".format(t)} t' # {"{0:.3f}".format(z)} use only 2 numbers after the dot in the t
-            self.writeValuesToExcel(sheet, sounding_number, z, t)
-            self.createGraph(x, y, sounding_number, z)
-            return answer_m3, answer_t
+            answer_V['text'] = f'You have {"{0:.3f}".format(z)} m3'  # {"{0:.3f}".format(z)} use only 2 numbers after the dot in the z2
+            # t = k * z  # Вычисленние тонн
+            self.writeValuesToExcel(sheet, sounding_number, z)
+            '''Mass calculation is an optional function, the customer can use it or not.
+               So if fields are empty or one of them is not specified then the calculation will not be worked'''
+            if calcMass:
+                self.displayMass(density, currentTemperature, z, answer_mass, sheet)
+            '''Add only unique tanks'''
+            if tank not in tanks_for_colors:
+                tanks_for_colors.append(tank)
+            print(tanks_for_colors)
+            self.createGraph(x, y, sounding_number, z, tank, sounding, tanks_for_colors)
+            return answer_V, tanks_for_colors
         else:
-            answer_m3['text'] = 'Uncorrected sounding, use format 0.00'
-            answer_t['text'] = ''
-            return answer_m3, answer_t
+            answer_V['text'] = 'Uncorrected sounding, use format 0.00'
+            answer_mass['text'] = ''
+            return answer_V, answer_mass
